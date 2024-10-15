a56344afdefafdb2c19a417c6c29f6b8eb91d234
IkJuLim <limik1004@gmail.com>
2024-10-16 00:51:56 +0900

    MyBatisConfig 생성

---
+++ src/main/java/com/limikju/daangn_market/config/MyBatisConfig.java
@@ -0,0 +1,36 @@
+package com.limikju.daangn_market.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(value = "com.limikju.daangn_market.repository", sqlSessionFactoryRef = "SqlSessionFactory")
+@EnableTransactionManagement
+public class MyBatisConfig {
+
+    @Value("${mybatis.mapper-locations}")
+    String mPath;
+
+    @Bean(name = "SqlSessionFactory")
+    public SqlSessionFactory SqlSessionFactory(@Qualifier("dataSource") DataSource DataSource, ApplicationContext applicationContext) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(DataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources(mPath));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean(name = "SessionTemplate")
+    public SqlSessionTemplate SqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory firstSqlSessionFactory) {
+        return new SqlSessionTemplate(firstSqlSessionFactory);
+    }
+}
