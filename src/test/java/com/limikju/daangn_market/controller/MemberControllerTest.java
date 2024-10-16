4c0124488e8491642e02118e0c3d8405f287cdd9
IkJuLim <limik1004@gmail.com>
2024-10-16 19:58:45 +0900

    MemberController test service 호출 확인 테스트

--- src/test/java/com/limikju/daangn_market/controller/MemberControllerTest.java
+++ src/test/java/com/limikju/daangn_market/controller/MemberControllerTest.java
@@ -1,7 +1,10 @@
 package com.limikju.daangn_market.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.limikju.daangn_market.domain.Member;
 import com.limikju.daangn_market.domain.dto.MemberSignUpDto;
+import com.limikju.daangn_market.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -25,11 +29,14 @@ class MemberControllerTest {
 
     @Autowired
     MockMvc mvc;
+    @Autowired
+    MemberRepository memberRepository;
 
     ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     @DisplayName("회원가입 성공")
+    @Transactional
     void signUpTest() throws Exception {
         //given
         MemberSignUpDto memberSignUpDto = MemberSignUpDto.builder()
@@ -40,12 +47,19 @@ class MemberControllerTest {
                 .address("서울시 강남구 역삼동")
                 .build();
 
-        //when, then
+        //when
         mvc.perform(
                         post("/api/members")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(memberSignUpDto)))
                 .andExpect(status().isCreated());
+
+        //then
+        Member member = memberRepository.findByEmail(memberSignUpDto.getEmail()).orElseThrow();
+        Assertions.assertThat(memberSignUpDto.getEmail()).isEqualTo(member.getEmail());
+        Assertions.assertThat(memberSignUpDto.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(memberSignUpDto.getPhone()).isEqualTo(member.getPhone());
+        Assertions.assertThat(memberSignUpDto.getAddress()).isEqualTo(member.getAddress());
     }
 
     @Test
