17acbb1f152ed6c2bb1d5c3b4ee61fe09a4c62aa
IkJuLim <limik1004@gmail.com>
2024-10-22 05:53:22 +0900

    login & logout test

--- src/main/java/com/limikju/daangn_market/domain/dto/MemberLoginDto.java
+++ src/main/java/com/limikju/daangn_market/domain/dto/MemberLoginDto.java
@@ -2,10 +2,14 @@ package com.limikju.daangn_market.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberLoginDto {
 
   //email
