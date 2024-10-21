fa8811d21dbb35aa3ac58a417426689d73b7ba5f
IkJuLim <limik1004@gmail.com>
2024-10-22 03:17:10 +0900

    redis 직렬화/역직렬화 에러 수정

--- src/main/java/com/limikju/daangn_market/config/RedisConfig.java
+++ src/main/java/com/limikju/daangn_market/config/RedisConfig.java
@@ -8,7 +8,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 
 @Configuration
 @EnableRedisRepositories
@@ -31,13 +33,18 @@ public class RedisConfig {
     return new LettuceConnectionFactory(redisStandaloneConfiguration);
   }
 
+  @Bean
+  public RedisIndexedSessionRepository sessionRepository(RedisTemplate<String, Object> redisTemplate) {
+    return new RedisIndexedSessionRepository(redisTemplate);
+  }
+
   // Redis template
   @Bean
   public RedisTemplate<?, ?> redisTemplate() {
     RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory());   //connection
     redisTemplate.setKeySerializer(new StringRedisSerializer());    // key
-    redisTemplate.setValueSerializer(new StringRedisSerializer());  // value
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());  // value
     return redisTemplate;
   }
 }
