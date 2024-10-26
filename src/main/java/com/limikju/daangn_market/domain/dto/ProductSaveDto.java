8f7d8f4f72d1f769585f448019c8f08bc6e2bc85
IkJuLim <limik1004@gmail.com>
2024-10-26 12:58:03 +0900

    Category Repository 에러 수정

--- src/main/java/com/limikju/daangn_market/domain/dto/ProductSaveDto.java
+++ src/main/java/com/limikju/daangn_market/domain/dto/ProductSaveDto.java
@@ -2,11 +2,15 @@ package com.limikju.daangn_market.domain.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductSaveDto {
 
   @NotBlank(message = "카테고리를 입력해주세요")
