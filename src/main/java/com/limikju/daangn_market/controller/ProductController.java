c42d81ee98ef42fdd43656cbde45ff240e0201f6
IkJuLim <limik1004@gmail.com>
2024-10-30 00:49:35 +0900

    단건 조회 Controller 구현 & Test

--- src/main/java/com/limikju/daangn_market/controller/ProductController.java
+++ src/main/java/com/limikju/daangn_market/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.limikju.daangn_market.controller;
 
+import com.limikju.daangn_market.domain.dto.ProductInfoDto;
 import com.limikju.daangn_market.domain.dto.ProductSaveDto;
 import com.limikju.daangn_market.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,11 @@ public class ProductController {
   public ResponseEntity<?> save(@RequestBody @Validated ProductSaveDto productSaveDto) {
     productService.save(productSaveDto);
     return new ResponseEntity(HttpStatus.CREATED);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> find(@PathVariable Long id) {
+    ProductInfoDto productInfoDto = productService.findById(id);
+    return ResponseEntity.ok(productInfoDto);
   }
 }
--- src/test/java/com/limikju/daangn_market/controller/ProductControllerTest.java
+++ src/test/java/com/limikju/daangn_market/controller/ProductControllerTest.java
@@ -2,10 +2,13 @@ package com.limikju.daangn_market.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.limikju.daangn_market.domain.dto.ProductInfoDto;
 import com.limikju.daangn_market.domain.dto.ProductSaveDto;
 import com.limikju.daangn_market.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
@@ -35,23 +38,18 @@ class ProductControllerTest {
   @Autowired
   private ObjectMapper objectMapper;
 
-  private ProductSaveDto productSaveDto;
 
-  @BeforeEach
-  void setUp() {
-    productSaveDto = ProductSaveDto.builder()
+  @Test
+  @DisplayName("상품 저장 성공")
+  @WithMockUser(username="test@test.com", password = "Q1w2e3r4!!", roles={"USER"})
+  void saveProduct_success() throws Exception {
+    // given
+    ProductSaveDto productSaveDto = ProductSaveDto.builder()
         .title("title")
         .content("content")
         .price(10000)
         .category("하의")
         .build();
-  }
-
-
-  @Test
-  @DisplayName("상품 저장 성공")
-  void saveProduct_success() throws Exception {
-    // given
 
     // when
     doNothing().when(productService).save(any(ProductSaveDto.class));
@@ -81,5 +79,29 @@ class ProductControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(productSaveDto)))
         .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  @DisplayName("상품 조회 성공")
+  @WithMockUser(username="test@test.com", password = "Q1w2e3r4!!", roles={"USER"})
+  void findProduct_success() throws Exception {
+    // given
+    ProductInfoDto productInfoDto = ProductInfoDto.builder()
+        .id(1L)
+        .title("title")
+        .content("content")
+        .price(10000)
+        .category("TestCategory")
+        .ownerId(1L)
+        .createdDate("2021-08-01")
+        .build();
+
+    // when
+    when(productService.findById(any(Long.class))).thenReturn(productInfoDto);
+
+    // then
+    mockMvc.perform(
+            get("/api/products/1"))
+        .andExpect(status().isOk());
   }
 }
