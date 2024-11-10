45eb4c1af0ddfc70dd48f63ee04b506f29c31477
IkJuLim <limik1004@gmail.com>
2024-11-11 01:17:38 +0900

    chat Service 생성

--- src/main/java/com/limikju/daangn_market/domain/Chat.java
+++ src/main/java/com/limikju/daangn_market/domain/Chat.java
@@ -2,21 +2,23 @@ package com.limikju.daangn_market.domain;
 
 import com.limikju.daangn_market.domain.enums.MessageType;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
+@Builder
 @Document(collation = "chat")
 public class Chat {
 
   @Id
   private String id;
-  private String sellerId;
-  private String buyerId;
-  private String productId;
-  private String senderId;
+  private Long sellerId;
+  private Long buyerId;
+  private Long productId;
+  private Long senderId;
   private MessageType type;
-  private String body;
+  private Object body;
   private LocalDateTime createdAt;
 }
--- src/main/java/com/limikju/daangn_market/repository/ChatRepository.java
+++ src/main/java/com/limikju/daangn_market/repository/ChatRepository.java
@@ -9,11 +9,11 @@ import reactor.core.publisher.Flux;
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 
   @Query("{ productId: ?0, buyerId: ?1 }")
-  Flux<Chat> findByProductIdAndBuyerId(String productId, String buyerId);
+  Flux<Chat> findByProductIdAndBuyerId(Long productId, Long buyerId);
 
   @Query("[{ $match: { $or: [ { sellerId: ?0, buyerId: ?1 }, { sellerId: ?1, buyerId: ?0 } ] } }, "
       +   "{ sort: { timestamp: -1 } }, "
       +   "{ $group: { _id: \"$product_id\", latestDocument: { $first: \"$$ROOT\" } } }, "
       +   "{ $replaceRoot: { newRoot: \"$latestDocument\" } } ]")
-  List<Chat> aggregateByProductIdAndSenderId(String productId, String senderId);
+  List<Chat> aggregateByProductIdAndSenderId(Long productId, Long senderId);
 }
