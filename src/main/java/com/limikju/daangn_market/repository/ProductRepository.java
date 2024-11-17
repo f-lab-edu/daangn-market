4d1a00c3dc001ddefe1cd8aceb64a726401510a7
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2024-11-17 18:59:22 +0900

    Revert "상품 정보 변경 구현"

--- src/main/java/com/limikju/daangn_market/repository/ProductRepository.java
+++ src/main/java/com/limikju/daangn_market/repository/ProductRepository.java
@@ -2,7 +2,6 @@ package com.limikju.daangn_market.repository;
 
 import com.limikju.daangn_market.domain.dto.ProductInfoDto;
 import com.limikju.daangn_market.domain.dto.ProductUpdateDto;
-import com.limikju.daangn_market.domain.enums.ProductStatus;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -13,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 public interface ProductRepository {
   public void save(Long ownerId, Long categoryId, String title, String content, int price);
   public Optional<ProductInfoDto> findById(Long id);
-  public void updateStatus(Long id, ProductStatus status);
   List<Map<String, Object>> getList(Pageable pageable);
   int getListCount();
   public void updateStatus(Long id);
