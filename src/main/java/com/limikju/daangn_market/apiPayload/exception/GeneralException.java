10cb0392f7fcf43bffbfdcfd4576ce53c5b1e6ab
IkJuLim <limik1004@gmail.com>
2024-12-05 09:42:48 +0900

    ApiPayload & ExceptionHandler

---
+++ src/main/java/com/limikju/daangn_market/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package com.limikju.daangn_market.apiPayload.exception;
+
+import com.limikju.daangn_market.apiPayload.code.BaseErrorCode;
+import com.limikju.daangn_market.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+  private BaseErrorCode code;
+
+  public ErrorReasonDTO getErrorReason() {
+    return this.code.getReason();
+  }
+
+  public ErrorReasonDTO getErrorReasonHttpStatus() {
+    return this.code.getReasonHttpStatus();
+  }
+}
